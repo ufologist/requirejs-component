bac5c135dce9731ffa5f6874b0f6537d8c88604f
ufologist <klsforever@gmail.com>
2017-09-22 09:59:52 +0800

    添加多环境配置的说明

--- require-base-config.js
+++ require-base-config.js
@@ -5,11 +5,21 @@
 // * map 公共组件的 URL 映射统一维护
 //   * 将全部的公共组件都在这里配置好, 这样一看便知有哪些公共组件, 而且其他项目也不需要逐一配置了
 //
-// 其他项目使用时, 先引入这个基础配置, 再按照需要添加或者覆盖一些配置
+// 其他项目使用时, 先引入这个基础配置, 可以再根据需要添加或者覆盖一些配置
 // 例如:
 // require.config({
 //     urlArgs: 'v=' +  Date.now()
 // });
+//
+// 如果需要支持多环境配置, 可以在这里定义一个全局的基础路径, 然后映射模块时拼接这个基础路径即可
+// 在页面中先定义基础路径即可控制使用不同的环境
+// 例如:
+// if (!window.componentBaseUrl) {
+//     window.componentBaseUrl = 'http://cdn.com/';
+// }
+// 
+// 在页面中我们先定义基础路径, 即可覆盖这个配置
+// <script>window.componentBaseUrl = 'http://test.cdn.com/';</script>
 require.config({
     // 如果使用了 data-main 则一定要记得设置 baseUrl, 因为使用了 data-main 会自动配置 baseUrl 为父级地址
     // 例如 data-main="index/index.js" 那么 baseUrl 就会被配置为 index/
